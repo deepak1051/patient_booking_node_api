bd7fe151bc831705a452c95511c48fae36ca18e6
deepak1051 <lightyagamigoku@gmail.com>
2024-04-07 21:28:06 +0530

    last commit

--- index.js
+++ index.js
@@ -21,27 +21,19 @@ mongoose
 // Define MongoDB schemas and models
 const ServiceSchema = new mongoose.Schema({
   name: String,
-  description: String,
-  duration: Number,
-});
-
-const DoctorSchema = new mongoose.Schema({
-  name: String,
-  specialization: String,
-  services: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Service' }],
+  doctorName: String,
 });
 
 const BookingSchema = new mongoose.Schema({
   patientName: String,
   phone: String,
   date: Date,
-
-  service: { type: mongoose.Schema.Types.ObjectId, ref: 'Service' },
-  doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor' },
+  service: String,
+  doctor: String,
 });
 
 const Service = mongoose.model('Service', ServiceSchema);
-const Doctor = mongoose.model('Doctor', DoctorSchema);
+
 const Booking = mongoose.model('Booking', BookingSchema);
 
 // Middleware to parse JSON requests
@@ -52,8 +44,25 @@ app.use(morgan('dev'));
 // Add a new service
 app.post('/services', async (req, res) => {
   try {
-    const { name, description, duration } = req.body;
-    const service = new Service({ name, description, duration });
+    const { name, doctorName } = req.body;
+
+    if (!name || !doctorName) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    const existingService = await Service.findOne({ name });
+
+    if (existingService) {
+      return res.status(400).json({ message: 'Service already exists' });
+    }
+
+    const existDoctor = await Service.findOne({ doctorName: doctorName });
+
+    if (existDoctor) {
+      return res.status(400).json({ message: 'Doctor already exists' });
+    }
+
+    const service = new Service({ name, doctorName });
     await service.save();
     res.status(201).json(service);
   } catch (err) {
@@ -66,28 +75,6 @@ app.get('/services', async (req, res) => {
   try {
     const services = await Service.find();
     res.json(services);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// Add a new doctor with respective services
-app.post('/doctors', async (req, res) => {
-  try {
-    const { name, specialization, services } = req.body;
-    const doctor = new Doctor({ name, specialization, services });
-    await doctor.save();
-    res.status(201).json(doctor);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// List all doctors with respective services
-app.get('/doctors', async (req, res) => {
-  try {
-    const doctors = await Doctor.find().populate('services');
-    res.json(doctors);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -114,8 +101,7 @@ app.post('/bookings', async (req, res) => {
     const booking = new Booking({
       patientName,
       phone,
-      bookingDate,
-      time,
+      date,
       service,
       doctor,
     });
